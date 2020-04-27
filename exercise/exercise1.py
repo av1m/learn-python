8e6c7dc3e3b078d246a4c2d50bc950e2af9a1561
Dylan Settbon <dylans_set@hotmail.fr>
2020-04-27 15:19:23 +0200

    [ADD]Exercise1

--- exercise/exercise1.py
+++ exercise/exercise1.py
@@ -1,13 +1,7 @@
-
+import requests,json
 
 def parse_github(username):
-    """
-    This function allows, according to a Github username, to return its node_id, its id as well as its name
+    contenu = json.loads(requests.get("https://api.github.com/users/"+username).content)
+    return [contenu["node_id"],contenu["id"],contenu["name"]]
 
-    Example:
-    parse_github("av1m") # ["MDQ6VXNlcjM2NDU2NzA5", 36456709, "Avi Mimoun"]
-
-    :param username: corresponds to the username Github
-    :return list: index 0 is node_id, index 1 is id, index 2 is name
-    """
-    pass
+parse_github("av1m")
